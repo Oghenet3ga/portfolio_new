6e567b29833a6b99268f6ab45457304f6adf18d1
Oghenetega Fregene <117751674+Oghenet3ga@users.noreply.github.com>
2023-06-13 12:55:02 +0100

    Changed font size and made footer responsive

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,13 @@ let typed = new Typed(".auto-input", {
   backSpeed: 100,
   loop: true,
 });
+
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  //reset: true//
+});
+sr.reveal(`.home_data`);
+//sr.reveal(`.home_image`, { origin: "bottom" });//
+sr.reveal(`.projects_card`);
