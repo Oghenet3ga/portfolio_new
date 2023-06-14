f44371b0defbc7790780c17e9db10ce7b41a07c2
Oghenetega Fregene <117751674+Oghenet3ga@users.noreply.github.com>
2023-06-14 10:35:43 +0100

    Added scrollreveal js

--- src/js/main.js
+++ src/js/main.js
@@ -13,4 +13,15 @@ const sr = ScrollReveal({
 });
 sr.reveal(`.home_data`);
 //sr.reveal(`.home_image`, { origin: "bottom" });//
-sr.reveal(`.projects_card`);
+sr.reveal(`.home_proj_image`, { origin: "left" });
+sr.reveal(`.project_description`, { origin: "right" });
+sr.reveal(`.about_image`, { origin: "right" });
+
+const pr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  //reset: true//
+});
+
+pr.reveal(`.about_image`, { origin: "right" });
